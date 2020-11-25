dc954fece83543f44d74444fbf03d40878ad604d
Owen Wexler <owenbwexler@gmail.com>
2020-11-24 22:24:06 -0500

    Refactor OMDB tests to eliminate UnhandledPromiseRejectionWarning

--- omdb/omdb.test.js
+++ omdb/omdb.test.js
@@ -2,12 +2,20 @@ const getOMDBMovie = require('./omdb.js');
 
 it('gets the title of a known movie successfully', () => {
   getOMDBMovie('Lean On Me', (err, result) => {
-    expect(result.Title).toBe('Lean On Me');
+    if(err) {
+      expect(result).toBeNull();
+    } else {
+      expect(result.Title).toBe('Lean On Me');
+    }
   })
 })
 
 it('returns a false response for an unknown movie', () => {
   getOMDBMovie('THISMOVIEDOESNOTEXIST', (err, result) => {
-    expect(result.Response).toBe('False');
+    if (err) {
+      expect(result).toBeNull();
+    } else {
+      expect(result.Response).toBe('False');
+    }
   })
 })
