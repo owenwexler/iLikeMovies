35aeffe463e55c2fe1e43ed65470841342978f55
Owen Wexler <owenbwexler@gmail.com>
2020-12-08 02:53:27 -0500

    Add files for watchedUnwatchedFilter and example state

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -1,6 +1,6 @@
 import { combineReducers } from 'redux';
-import movies from './movieReducer';
+import watchedUnwatchedFilterReducer from './watchedUnwatchedFilterReducer.js'
 
 export default combineReducers({
-  movies
+  watchedUnwatchedFilterReducer
 })
