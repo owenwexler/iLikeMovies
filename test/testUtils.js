6ebacea1ee425cc9b8d824b2cda088533d156ecf
Owen Wexler <owenbwexler@gmail.com>
2020-12-08 03:35:52 -0500

    Replace omdb.test.js and server.test.js

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,20 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore, applyMiddleware } from 'redux';
+
+import rootReducer from '../client/src/reducers/rootReducer.js';
+import { middlewares } from '../client/src/configureStore';
+
+/**
+ * Create a testing store with imported reducers, middleware, and initial state.
+ *  globals: rootReducer, middlewares.
+ * @param {object} initialState - Initial state for store.
+ * @function storeFactory
+ * @returns {Store} - Redux store.
+ */
+export const storeFactory = (initialState) => {
+  const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore)
+  return createStoreWithMiddleware(rootReducer, initialState);
+}
 
 /**
  * Return node(s) with the given data-test attribute.
