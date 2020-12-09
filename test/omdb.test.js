5f8bf26e9aaec6569a9edc429b13a1240a31b7fc
Owen Wexler <owenbwexler@gmail.com>
2020-12-09 00:45:32 -0500

    Restore OMDB and server tests to test folder

---
+++ test/omdb.test.js
@@ -0,0 +1,21 @@
+const getOMDBMovie = require('../omdb/omdb.js');
+
+it('gets the title of a known movie successfully', () => {
+  getOMDBMovie('Lean On Me', (err, result) => {
+    if(err) {
+      expect(result).toBeNull();
+    } else {
+      expect(result.Title).toBe('Lean On Me');
+    }
+  })
+})
+
+it('returns a false response for an unknown movie', () => {
+  getOMDBMovie('THISMOVIEDOESNOTEXIST', (err, result) => {
+    if (err) {
+      expect(result).toBeNull();
+    } else {
+      expect(result.Response).toBe('False');
+    }
+  })
+})
