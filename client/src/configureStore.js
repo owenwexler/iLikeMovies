0ced82acee67e1bb667cce69b7826f9fe4a9de21
Owen Wexler <owenbwexler@gmail.com>
2020-12-10 00:22:49 -0500

    Restore default export in store configuration

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -3,3 +3,6 @@ import ReduxThunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer.js';
 
 export const middlewares = [ReduxThunk];
+const createStoreWithMiddleware = applyMiddleware(...middlewares)(createStore);
+
+export default createStoreWithMiddleware(rootReducer);
