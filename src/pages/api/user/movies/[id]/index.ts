70acea71b184edb00bb1dbdf39df75fa9dc42a66
Owen Wexler <owenbwexler@gmail.com>
2024-10-15 20:15:17 -0400

    Begin adding functionality for getting user movies (fix needed for movies not in OMDB)

--- src/pages/api/user/movies/[id]/index.ts
+++ src/pages/api/user/movies/[id]/index.ts
@@ -1,12 +1,13 @@
 import type { APIRoute } from 'astro';
-import { getOMDBMovie } from '../../../../../omdb/omdb';
-import type { UserMovie } from '../../../../db/schema';
+import { getUserMoviesById } from '../../../../../db/models/userMovies';
+import type { UserMovie } from '../../../../../db/schema';
+import { getOMDBMovie } from '../../../../../../omdb/omdb';
 
-export const GET: APIRoute = async ({ request }) => {
-  const searchParams = new URL(request.url).searchParams;
-  const idParam = searchParams.get('id');
-  const id = idParam ? idParam as string : '';
+export const GET: APIRoute = async ({ params, request }) => {
+  const { id } = params;
   
+  console.log('id: ', id);
+
   const env = import.meta.env;
   const omdbAPIKey = env.OMDB_API_KEY;
   const nodeEnv = env.NODE_ENV ? env.NODE_ENV : 'development';
@@ -15,10 +16,13 @@ export const GET: APIRoute = async ({ request }) => {
   try {
     const userMovieResponse: UserMovie = await getUserMoviesById({ inputUserId: id });
 
+    console.log('userMovieResponse: ', userMovieResponse);
+
     const promises = [];
 
     for (const userMovie of userMovieResponse) {
-      promises.push(getOMDBMovie({ movieTitle: userMovie.title, apiKey: omdbAPIKey, devMode: devMode, method: 'title' }));
+      // TODO: figure out why titles not in OMDB aren't going through
+      promises.push(getOMDBMovie({ movieTitle: userMovie.movieName.toLowerCase(), apiKey: omdbAPIKey, devMode: devMode, method: 'title' }));
       /*
        * TODO: add IMDB IDs for each movie to omdbOffline and uncomment this code when this is done
       if (userMovie.imdbId !== null) {
@@ -28,6 +32,11 @@ export const GET: APIRoute = async ({ request }) => {
       }
       */
     }
+
+    const data = await Promise.all(promises);
+    console.log('promises from api route: ', promises)
+
+    return new Response(JSON.stringify(data));
   } catch (error) {
     console.error(error);
     throw error;
